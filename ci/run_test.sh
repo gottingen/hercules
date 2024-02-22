9bdb3e916796de070d66fd1e56875caf120806ac
jeff.li <lijippy@163.com>
2024-02-23 01:43:00 +0800

    add build installer

---
+++ ci/run_test.sh
@@ -0,0 +1,44 @@
+#! /usr/bin/env bash
+# Copyright 2023 The titan-search Authors.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+set -xue
+set -o pipefail
+
+THIS_PATH=$(cd $(dirname "$0"); pwd)
+ROOT_PATH=${THIS_PATH}/../
+BUILD_PATH=${ROOT_PATH}/build
+
+# mkdir lib
+if [ ! -d "${BUILD_PATH}" ]; then
+  mkdir -p "${BUILD_PATH}"
+else
+  rm -rf "${BUILD_PATH:?}/*"
+fi
+
+# build test
+cd "${BUILD_PATH}"
+cmake ../
+make -j8
+
+# export env
+PYTHONLIB=`find_libpython`
+export HERCULES_PATH=${PYTHONLIB}
+export PYTHONPATH=${ROOT_PATH}/test/python
+cd "${ROOT_PATH}"
+./build/hercules_test
+
+
+
