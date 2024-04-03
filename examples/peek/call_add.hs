64f9d96cf25f6f1aaca4251277af91497830b4dd
jeff.li <lijippy@163.com>
2024-04-03 16:37:55 +0800

    add docs

--- examples/peek/call_add.hs
+++ examples/peek/call_add.hs
@@ -3,6 +3,6 @@ from add import simple_add
 
 print(simple_add(3,2))
 
-LIB="./libadd_shared.so"
-from C import LIB.shared_add(int, int)->int
+#LIB="./libadd_shared.so"
+from C import shared_add(int, int)->int
 print(shared_add(3,2))
